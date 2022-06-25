f633e844d370ca8cf6554bfe9196d295c07f86cc
Kristina Krylova <kriskamumu@gmail.com>
2022-06-25 21:51:47 +0300

    added AbstractRestAssuredClient and AuthClient in client package

---
+++ src/main/java/ru/yandex/burgers/client/AbstractRestAssuredClient.java
@@ -0,0 +1,14 @@
+package ru.yandex.burgers.client;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+abstract class AbstractRestAssuredClient {
+    protected RequestSpecification getBaseSpec() {
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri("https://stellarburgers.nomoreparties.site/")
+                .build();
+    }
+}
---
+++ src/main/java/ru/yandex/burgers/client/AuthClient.java
@@ -0,0 +1,46 @@
+package ru.yandex.burgers.client;
+
+import io.restassured.response.ValidatableResponse;
+import ru.yandex.burgers.model.AuthorizedUser;
+import ru.yandex.burgers.model.User;
+import ru.yandex.burgers.model.UserCredentials;
+
+import static io.restassured.RestAssured.given;
+
+public class AuthClient extends AbstractRestAssuredClient {
+
+    private final static String AUTH_PATH = "/api/auth";
+
+    public ValidatableResponse register(User user){
+        return given()
+                .spec(getBaseSpec())
+                .body(user)
+                .when()
+                .log().all()
+                .post(AUTH_PATH + "/register")
+                .then()
+                .log().all();
+    }
+
+    public ValidatableResponse login(UserCredentials userCredentials){
+        return given()
+                .spec(getBaseSpec())
+                .body(userCredentials)
+                .when()
+                .log().all()
+                .post(AUTH_PATH + "/login")
+                .then()
+                .log().all();
+    }
+
+    public ValidatableResponse delete(AuthorizedUser authorizedUser){
+        return given()
+                .spec(getBaseSpec())
+                .header("Authorization", authorizedUser.getAccessToken())
+                .when()
+                .log().all()
+                .delete(AUTH_PATH + "/user")
+                .then()
+                .log().all();
+    }
+}
