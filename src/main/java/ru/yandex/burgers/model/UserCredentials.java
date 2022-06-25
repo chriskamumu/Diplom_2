f88cef45c2c5258d61e7934e76a8f2f5dbaf51d4
Kristina Krylova <kriskamumu@gmail.com>
2022-06-25 21:51:05 +0300

    added User, UserCredentials and AuthorizedUser classed in model package

---
+++ src/main/java/ru/yandex/burgers/model/UserCredentials.java
@@ -0,0 +1,20 @@
+package ru.yandex.burgers.model;
+
+public class UserCredentials {
+
+    private final String email;
+    private final String password;
+
+    public UserCredentials(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
