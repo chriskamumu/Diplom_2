12842d6eba8313d6a11d9fa0da76c120ad6f6392
Kristina Krylova <kriskamumu@gmail.com>
2022-07-05 21:59:42 +0300

    added lombok

--- src/main/java/ru/yandex/burgers/model/Order.java
+++ src/main/java/ru/yandex/burgers/model/Order.java
@@ -1,26 +1,15 @@
 package ru.yandex.burgers.model;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class Order {
     private final List<String> ingredients;
-
-    public List<String> getIngredients() {
-        return ingredients;
-    }
-
-    public Order(List<String> ingredients) {
-        this.ingredients = ingredients;
-    }
 
     public void addIngredient(String ingredient){
         ingredients.add(ingredient);
     }
 
-    @Override
-    public String toString() {
-        return "Order{" +
-                "ingredients=" + ingredients +
-                '}';
-    }
 }
--- src/main/java/ru/yandex/burgers/model/UserCredentials.java
+++ src/main/java/ru/yandex/burgers/model/UserCredentials.java
@@ -1,28 +1,10 @@
 package ru.yandex.burgers.model;
 
+import lombok.Data;
+
+@Data
 public class UserCredentials {
 
     private final String email;
     private final String password;
-
-    public UserCredentials(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String toString() {
-        return "UserCredentials{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
